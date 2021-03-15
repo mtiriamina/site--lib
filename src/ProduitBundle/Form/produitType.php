7e9366ab8097a6d5a228718db5da56cce3a34547
MO7S3N <34940993+MO7S3N@users.noreply.github.com>
2021-03-15 17:17:22 +0100

    projet
    proojet

---
+++ src/ProduitBundle/Form/produitType.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ProduitBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class produitType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('nomP')
+            ->add('prix')
+            ->add('quantite')
+            ->add('description')
+            ->add('categorie',EntityType::class, array(
+                'choice_label'=>'nomCat',
+                'class'=>'CategorieBundle\Entity\categorie'
+            ))
+            ->add('imageFile',VichImageType::class)
+        ;
+    }
+
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'ProduitBundle\Entity\produit'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'produitbundle_produit';
+    }
+
+
+}
